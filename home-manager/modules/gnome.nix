22666126a3c439fdffb189088e3529218a534470
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-02-16 22:36:21 +0500

    add blur extension to gnome

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -4,7 +4,7 @@
       x11.enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
-      size = 16;
+      size = 17;
   };
 
   gtk = {
@@ -16,13 +16,29 @@
     };
 
     iconTheme = {
-      package = pkgs.adwaita-icon-theme;
-      name = "Adwaita";
+      package = pkgs.whitesur-icon-theme;
+      name = "WhiteSur-Dark";
     };
 
     font = {
       name = "Sans";
       size = 11;
+    };
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false; 
+        enabled-extensions = [
+          "blur-my-shell@anet"
+        ];
+      };
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        brightness = 0.75;
+        noise-amount = 0;
+      };
     };
   };
 }
