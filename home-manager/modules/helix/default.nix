203bc0f693991a0009e35976871c3e3f90ac47b7
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-03-19 22:34:28 +0330

    refactor helix configs

--- home-manager/modules/helix/default.nix
+++ home-manager/modules/helix/default.nix
@@ -1,7 +1,12 @@
 {
   home.file = {
     ".config/helix/themes/skull.toml" = {
-        source = "skull.toml";
+        source = ./skull.toml;
+        recursive = true;
+    };
+
+    ".config/helix/config.toml" = {
+        source = ./config.toml;
         recursive = true;
     };
   }; 
