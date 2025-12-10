41034b777e0dc3405e1aad7a011b3d142c6de5ef
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-12-10 23:22:06 +0330

    add new host

---
+++ hosts/yhwach/configuration.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, stateVersion, hostname, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../nixos/modules
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  programs.nix-ld.enable = true;
+  
+  networking.hostName = hostname;
+  system.stateVersion = stateVersion;
+
+  hardware.graphics = { 
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    home-manager
+    cudaPackages.cudatoolkit
+  ];
+}
+
